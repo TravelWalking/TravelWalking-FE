62d6637b9d4e3ab3850bc1271bbf98d9e8236cfc
daeun088 <kde2329@naver.com>
2024-07-15 21:50:21 +0900

    init : 네비게이션, eslint, prettier 세팅

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,32 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    '@react-native-community',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:prettier/recommended', 
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'prettier'
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: 'avoid',
+        bracketSameLine: true,
+        bracketSpacing: false,
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+  },
 };
--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
 
 const App = () => {
