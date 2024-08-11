398964782710979da4e5a806f687ccfcf78b898e
daeun088 <kde2329@naver.com>
2024-08-11 15:27:28 +0900

    feat : 기록 추가 버튼

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
 import Dropdown from '../components/Dropdown';
 
 const HomeScreen = () => {
@@ -7,19 +7,24 @@ const HomeScreen = () => {
 
   return (
     <View style={styles.container}>
+      {/* MapView 또는 FeedView를 먼저 렌더링 */}
+      {selectedOption === '지도로 보기' && <MapView />}
+      {selectedOption === '피드로 보기' && <FeedView />}
+      
+      {/* 드롭다운 헤더 */}
       <View style={styles.dropdownContainer}>
         <Dropdown selectedOption={selectedOption} setSelectedOption={setSelectedOption} />
       </View>
-      {/* 선택된 옵션에 따라 다른 컴포넌트 렌더링 */}
-      {selectedOption === '지도로 보기' && <MapView />}
-      {selectedOption === '피드로 보기' && <FeedView />}
+      <TouchableOpacity style={styles.fixedButton}>
+        <Text style={styles.buttonText}>기록 추가하기</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
 const MapView = () => {
   return (
-    <View style={styles.content}>
+    <View style={styles.mapView}>
       <Text>여기에 지도 화면이 표시됩니다.</Text>
     </View>
   );
@@ -27,7 +32,7 @@ const MapView = () => {
 
 const FeedView = () => {
   return (
-    <View style={styles.content}>
+    <View style={styles.feedView}>
       <Text>여기에 피드 화면이 표시됩니다.</Text>
     </View>
   );
@@ -36,25 +41,40 @@ const FeedView = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    position: 'relative',
     backgroundColor: '#fff',
   },
   dropdownContainer: {
     position: 'absolute',
-    top: 0,
-    left: 0,
+    zIndex: 10,  // 다른 뷰들보다 위에 오도록 설정
   },
-  title: {
-    fontSize: 24,
+  mapView: {
+    ...StyleSheet.absoluteFillObject,  // 전체 화면을 채우도록 설정
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#e0f7fa',  // 예시로 배경색 설정
+  },
+  feedView: {
+    ...StyleSheet.absoluteFillObject,  // 전체 화면을 채우도록 설정
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fce4ec',  // 예시로 배경색 설정
+  },
+  fixedButton: {
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    bottom: 20,
+    alignSelf: 'center',
+    backgroundColor: '#DAE628',
+    padding: 10,
+    paddingHorizontal: 15,
+    borderRadius: 20,
+  },
+  buttonText: {
+    fontSize: 16,
+    color: '#000',
     fontWeight: 'bold',
-  },
-  content: {
-    marginTop: 20,
-    padding: 20,
-    borderWidth: 1,
-    borderColor: '#ddd',
-    borderRadius: 5,
   },
 });
 
