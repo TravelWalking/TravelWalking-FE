3ea057cfd7b66a8c4a9bfeb823a5bf664ac5d28e
daeun088 <kde2329@naver.com>
2024-08-25 22:38:32 +0900

    chore : ScroolView 추가

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,11 +1,18 @@
 // SearchScreen.tsx
 import React from 'react';
 import { View, Text } from 'react-native';
+import SearchBox from '../components/SearchBox';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const SearchScreen = () => {
   return (
     <View>
-      <Text>Search Screen</Text>
+      <Text>검색어를 입력하세요.</Text>
+      <SearchBox/>
+      <Text>최근 검색어</Text>
+      <ScrollView></ScrollView>
+      
+      
     </View>
   );
 };
